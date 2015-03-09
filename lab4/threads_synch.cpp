0159ed3ff5d1b90dec021d7f4ef9da5f50f52815
AntonPashkowskiy <Wolendrang@gmail.com>
2015-03-09 22:04:48 +0300

    Work with thread (2)

--- lab4/threads_synch.cpp
+++ lab4/threads_synch.cpp
@@ -1,12 +1,23 @@
 #include "threads_synch.h"
 #include <iostream>
 #include <string.h>
+#include <stdio.h>
 
 using namespace std;
+
+//убиваем все процессы и уничтожаем мьютекс
+//далее освобождаем память выделенную под строчки
 
 ThreadSynch::~ThreadSynch()
 {
     KillAllThreads();
+    pthread_mutex_destroy( &_mutex );
+
+    while( _uStringPointers.size() != 0 )
+    {
+        delete [] _uStringPointers.back();
+        _uStringPointers.pop_back();
+    }
 }
 
 //String contents one-symbol characters:
@@ -52,6 +63,11 @@ bool ThreadSynch::CheckCommandString()
 
 bool ThreadSynch::InitMutex()
 {
+    if( pthread_mutex_init( &_mutex, NULL ) != 0 )
+    {
+        return false;
+    }
+
     return true;
 }
 
@@ -60,25 +76,127 @@ bool ThreadSynch::Execute()
     return false;
 }
 
-void ThreadSynch::AddThread()
+bool ThreadSynch::AddThread()
 {
+    ThreadArgumets* arguments = FormArgumetns();
+    pthread_t threadID;
 
+    if( arguments == NULL )
+    {
+        return false;
+    }
+
+    int check = pthread_create( &threadID, NULL, ShowUniqueString, arguments );
+
+    if( check != 0 )
+    {
+        return false;
+    }
+
+    _threadsID.push_back( threadID );
+    return true;
+}
+
+ThreadArgumets* ThreadSynch::FormArgumetns()
+{
+    ThreadArgumets* arguments = new ThreadArgumets;
+
+    arguments -> mutex = &_mutex;
+    arguments -> uniqueString = CreateUniqueString();
+
+    if( arguments -> uniqueString == NULL )
+    {
+        return NULL;
+    }
+
+    return arguments;
+}
+
+char* ThreadSynch::CreateUniqueString()
+{
+    char* uniqueString = new char[ 30 ];
+    char* number = new char[ 4 ];
+
+    strcpy( uniqueString, "I'm thread number " );
+    sprintf( number, "%d", _threadsID.size() );
+    strcat( uniqueString, number );
+
+    //добавляем указатель в список.
+    _uStringPointers.push_back( uniqueString );
+
+    delete [] number;
+    return uniqueString;
 }
 
 void ThreadSynch::KillLastThread()
 {
+    pthread_mutex_lock( &_mutex );
 
+    if( _threadsID.size() != 0 )
+    {
+        pthread_t lastThread = _threadsID.back();
+
+        if (pthread_kill( lastThread, 0) != ESRCH )
+        {
+            // посылаем последнему потоку в списке
+            // служебный сигнал, который ее завершает
+            pthread_cancel( lastThread );
+
+            // закрываем дескриптор thread_child */
+            pthread_join( lastThread, 0x00);
+        }
+
+        _threadsID.pop_back();
+
+        //освобождаем строчку которую использовал поток
+        char* stringPointer = _uStringPointers.back();
+        _uStringPointers.pop_back();
+        delete [] stringPointer;
+    }
+
+    pthread_mutex_unlock( &_mutex );
 }
 
 void ThreadSynch::KillAllThreads()
 {
-
+    while( _threadsID.size() != 0 )
+    {
+        KillLastThread();
+    }
 }
 
 //---------------- Thread function -----------------
 
 void* ShowUniqueString( void* arguments )
 {
+    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, 0x00);
+    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, 0x00);
+
+    ThreadArgumets* args = (ThreadArgumets*)argumetns;
+    int stringLength = 0;
+
+    if( args -> uniqueString != NULL && args -> mutex != NULL )
+    {
+        stringLength = strlen( args -> uniqueString );
+    }
+    else
+    {
+        return NULL;
+    }
+
+    while( true )
+    {
+        pthread_mutex_lock( args -> mutex );
+
+        for( int i = 0; i < stringLength; i++ )
+        {
+            cout << ( args -> uniqueString[ i ] );
+        }
+
+        cout << endl;
+        pthread_mutex_unlock( args -> mutex );
+        sleep( 1 );
+    }
 
 }
 
--- lab4/threads_synch.h
+++ lab4/threads_synch.h
@@ -1,26 +1,39 @@
 #if !defined( THREADS_SYNCH )
 #define THREADS_SYNCH
 #include <pthread.h>
-#include <vector>
+#include <list>
 #endif
+
+struct ThreadArgumets
+{
+    pthread_mutex_t* mutex;
+    char* uniqueString;
+};
 
 void* ShowUniqueString( void* arguments );
 
 class ThreadSynch
 {
     private:
-        std::vector< pthread_t > _threadsID;
+        std::list< pthread_t > _threadsID;
+        std::list<char*> _uStringPointers; //указатели на уникальные строчки потоков
+        pthread_mutex_t _mutex;
         char _commandString[ 50 ];
+
         bool CheckCommandString();
+        char* CreateUniqueString();
+        ThreadArgumets* FormArgumetns();
 
     public:
         ~ThreadSynch();
         void EnterCommandString();
         bool InitMutex();
         bool Execute();
-        void AddThread();
+        bool AddThread();
         void KillLastThread();
         void KillAllThreads();
 };
+
+
 
 
